ca71e82c5c4d7810514ad729a796815d28313807
Damp-Otter <tom.e.j.sutton@gmail.com>
2026-04-14 19:08:33 +0100

    Server side networking

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -5,31 +5,39 @@ using Unity.Netcode;
 public class PlayerController : NetworkBehaviour
 {
 
-    private PlayerInputController inputController;
+    //private PlayerInputController inputController;
     private float speed = 15f;
-    private NetworkVariable<Vector2> _input = null;
+    private NetworkVariable<Vector2> _input = new NetworkVariable<Vector2>(Vector2.zero,
+    NetworkVariableReadPermission.Everyone,
+    NetworkVariableWritePermission.Owner);
 
     private void Awake()
     {
-        inputController = GetComponent<PlayerInputController>();
-        _input = new NetworkVariable<Vector2>(inputController.MovementInputVector,
-            NetworkVariableReadPermission.Everyone, 
-            NetworkVariableWritePermission.Owner);
+        //inputController = GetComponent<PlayerInputController>();
     }
 
 
     private void Update()
     {
+        if (IsServer)
+        {
+            Vector3 positionChange = new Vector3(
+                _input.Value.x,
+                0,
+                _input.Value.y);
 
-        
+            transform.position += positionChange * Time.deltaTime * speed;
+        }
 
-        Vector3 positionChange = new Vector3(
-            inputController.MovementInputVector.y, 
-            0,
-            -inputController.MovementInputVector.x);
-
-        transform.position += positionChange * Time.deltaTime * speed;
     }
 
+    private void OnMove(InputValue inputValue)
+    {
+        if (IsOwner)
+        {
+            _input.Value = inputValue.Get<Vector2>().normalized;
+        }
+
+    }
 
 }
